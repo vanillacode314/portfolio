5e79a276585508110d5ec1d7ae5e4634874f448a
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-03-17 19:51:36 +0530

    fix broken discord link

--- src/lib/layouts/Links.svelte
+++ src/lib/layouts/Links.svelte
@@ -11,12 +11,16 @@
 			<a rel="external" href="https://gitlab.com/vanillacode">Gitlab (vanillacode)</a>
 		</li>
 		<li>
-			<a rel="external" href="https://discordapp.com/users/943908202846756874/"
-				>Discord (Raqueebuddin Aziz#6187)</a
-			>
+			<a rel="external" href="https://t.me/vanillacode314">Telegram (vanillacode314)</a>
 		</li>
 		<li>
-			<a rel="external" href="https://t.me/vanillacode314">Telegram (vanillacode314)</a>
+			<a rel="external" href="https://discord.com/app">Discord (Raqueebuddin Aziz#6187)</a>
+			<small>
+				<em
+					>(This is not a link to my discord profile it will redirect you to discord you would need
+					to enter my discord tag to add me as a friend)</em
+				>
+			</small>
 		</li>
 	</ul>
 </Card>
