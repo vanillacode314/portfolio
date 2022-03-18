d43d5d60604ea1b642299cf1dec53bb7cca7ef9f
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-03-18 17:25:37 +0530

    add fb messenger link

--- src/lib/layouts/Links.svelte
+++ src/lib/layouts/Links.svelte
@@ -1,18 +1,33 @@
 <script lang="ts">
 	import { Card } from 'spaper';
+
+	const links = [
+		{
+			url: 'https://github.com/vanillacode314',
+			label: 'Github (vanillacode314)'
+		},
+		{
+			url: 'https://gitlab.com/vanillacode',
+			label: 'Gitlab (vanillacode)'
+		},
+		{
+			url: 'https://t.me/vanillacode314',
+			label: 'Telegram (vanillacode314)'
+		},
+		{
+			url: 'https://m.me/vanillacode314',
+			label: 'Facebook (vanillacode314)'
+		}
+	];
 </script>
 
 <Card header="Links" id="links">
 	<ul>
-		<li>
-			<a rel="external" href="https://github.com/vanillacode314">Github (vanillacode314)</a>
-		</li>
-		<li>
-			<a rel="external" href="https://gitlab.com/vanillacode">Gitlab (vanillacode)</a>
-		</li>
-		<li>
-			<a rel="external" href="https://t.me/vanillacode314">Telegram (vanillacode314)</a>
-		</li>
+		{#each links as link (link.url)}
+			<li>
+				<a rel="external" href={link.url}>{link.label}</a>
+			</li>
+		{/each}
 		<li>
 			<a rel="external" href="https://discord.com/app">Discord (Raqueebuddin Aziz#6187)</a>
 			<small>
