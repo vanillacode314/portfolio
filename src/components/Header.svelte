45f65e26f41869e9d96afb1c88e83c097a6a9ec6
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-07-26 21:55:19 +0530

    header font improvements

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -35,17 +35,17 @@
 			<div class="flex flex-col mt-2 -mx-2 lg:mt-0 lg:flex-row lg:block" class:hidden={!menuOpen}>
 				<a
 					href="#testimonials"
-					class="px-2 py-2 text-sm text-gray-800 rounded-md dark:text-gray-200 hover:bg-gray-900 hover:text-gray-100 hover:font-medium lg:mx-2"
+					class="px-2 py-2 text-xs text-gray-800 rounded-md dark:text-gray-200 hover:bg-gray-900 hover:text-gray-100 hover:font-semibold lg:mx-2 uppercase font-medium"
 					>Testimonials</a
 				>
 				<a
 					href="#projects"
-					class="px-2 py-2 text-sm text-gray-800 rounded-md dark:text-gray-200 hover:bg-gray-900 hover:text-gray-100 hover:font-medium lg:mx-2"
+					class="px-2 py-2 text-xs text-gray-800 rounded-md dark:text-gray-200 hover:bg-gray-900 hover:text-gray-100 hover:font-semibold lg:mx-2 uppercase font-medium"
 					>Projects</a
 				>
 				<a
 					href="#contact"
-					class="px-2 py-2 text-sm text-gray-800 rounded-md dark:text-gray-200 hover:bg-gray-900 hover:text-gray-100 hover:font-medium lg:mx-2"
+					class="px-2 py-2 text-xs text-gray-800 rounded-md dark:text-gray-200 hover:bg-gray-900 hover:text-gray-100 hover:font-semibold lg:mx-2 uppercase font-medium"
 					>Contact</a
 				>
 			</div>
