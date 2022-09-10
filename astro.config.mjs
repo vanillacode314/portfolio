f6ca32cd372dfdcf4cac41ee547c225788cc9109
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-09-10 18:42:49 +0530

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
-
 import sitemap from "@astrojs/sitemap";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   // Enable Svelte to support Svelte components.
   site: "https://raqueebuddinaziz.com",
   integrations: [svelte(), sitemap()],
+  output: "server",
+  adapter: netlify()
 });
