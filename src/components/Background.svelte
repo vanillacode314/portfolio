e6d660b587515de38775f2be7ef169050b51a6a2
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-09-11 22:07:58 +0530

    feat: add parallax scrolling

--- src/components/Background.svelte
+++ src/components/Background.svelte
@@ -1,36 +1,52 @@
+<script lang="ts">
+  function updateParallax() {
+    const percentScrolled =
+      document.scrollingElement.scrollTop /
+      (document.scrollingElement.scrollHeight -
+        document.scrollingElement.clientHeight);
+    document.body.style.setProperty(
+      "--percent-scrolled",
+      percentScrolled + "px"
+    );
+  }
+</script>
+
+<svelte:window on:scroll={updateParallax} />
+
 <div class="container">
-      <div
-            class="bg-box"
-            style="background-color: var(--color-3); top: 20%; left:0;"
-      />
-      <div
-            class="bg-box"
-            style="background-color: var(--color-4); right: 0; top:-200px"
-      />
-      <div
-            class="bg-box"
-            style="background-color: var(--color-2); right: 0; top:50%;"
-      />
+  <div
+    class="bg-box"
+    style="background-color: var(--color-3); top: 20%; left:0;"
+  />
+  <div
+    class="bg-box"
+    style="background-color: var(--color-4); right: 0; top:-200px"
+  />
+  <div
+    class="bg-box"
+    style="background-color: var(--color-2); right: 0; top:50%;"
+  />
 </div>
 
 <style>
-      .container {
-            position: fixed;
-            inset: 0;
+  .container {
+    position: fixed;
+    inset: 0;
+  }
+  .bg-box {
+    z-index: var(--bg-layer);
+    position: absolute;
+    border-radius: var(--radius);
+    height: 300px;
+    width: 300px;
+    transform: translateY(calc(var(--percent-scrolled) * 100))
+      rotate(60deg);
+    filter: blur(10px);
+    opacity: 75%;
+    &:first-child {
+      @media (--md-n-below) {
+        display: none;
       }
-      .bg-box {
-            z-index: var(--bg-layer);
-            position: absolute;
-            border-radius: var(--radius);
-            height: 300px;
-            width: 300px;
-            transform: rotate(60deg);
-            filter: blur(10px);
-            opacity: 75%;
-            &:first-child {
-                  @media (--md-n-below) {
-                        display: none;
-                  }
-            }
-      }
+    }
+  }
 </style>
