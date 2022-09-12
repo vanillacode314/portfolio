8677fd278f682fdbcdaa3ac917d715dba642c47d
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-09-12 10:57:12 +0530

    chore: only use postcss-stylelint on build

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,22 @@
 const postcssJitProps = require("postcss-jit-props");
 const OpenProps = require("open-props");
+
+const plugins = [];
+
+process.env.MODE === "PROD" &&
+  plugins.push(require("stylelint")({ fix: true }));
+
+plugins.push(
+  require("postcss-nested"),
+  postcssJitProps(OpenProps),
+  require("postcss-custom-media")({
+    importFrom: "./node_modules/open-props/media.min.css",
+  }),
+  require("autoprefixer"),
+  require("cssnano"),
+  require("postcss-reporter")({ clearReportedMessages: true })
+);
+
 module.exports = {
-  plugins: [
-    require("stylelint")({ fix: true }),
-    require("postcss-nested"),
-    postcssJitProps(OpenProps),
-    require("postcss-custom-media")({
-      importFrom: "./node_modules/open-props/media.min.css",
-    }),
-    require("autoprefixer"),
-    require("cssnano"),
-    require("postcss-reporter")({ clearReportedMessages: true }),
-  ],
+  plugins,
 };
