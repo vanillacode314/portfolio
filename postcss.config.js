32a885b4b382d81f440d519eb40b67694a229414
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-09-12 08:46:09 +0530

    chore: add stylelint postcss plugin

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ const postcssJitProps = require("postcss-jit-props");
 const OpenProps = require("open-props");
 module.exports = {
   plugins: [
+    require("stylelint")({ fix: true }),
     require("postcss-nested"),
     postcssJitProps(OpenProps),
     require("postcss-custom-media")({
@@ -9,5 +10,6 @@ module.exports = {
     }),
     require("autoprefixer"),
     require("cssnano"),
+    require("postcss-reporter")({ clearReportedMessages: true }),
   ],
 };
