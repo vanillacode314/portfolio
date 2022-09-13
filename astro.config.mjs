9e70a2f6e7648558dd4e0e13c1d6e3e393a6f1bc
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-09-13 22:06:55 +0530

    chore: update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,32 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import sitemap from "@astrojs/sitemap";
+import fs from "node:fs";
+
+import netlify from "@astrojs/netlify/functions";
+const BASE_URL = "https://raqueebuddinaziz.com";
+
+function getBlogs() {
+  const files = fs.readdirSync("src/pages/blog");
+  const blogs = files.filter((file) => file.endsWith(".md"));
+  return blogs.map((blog) => BASE_URL + "/blog/" + blog.replace(".md", ""));
+}
 
 // https://astro.build/config
 export default defineConfig({
   // Enable Svelte to support Svelte components.
-  site: "https://raqueebuddinaziz.com",
-  integrations: [svelte(), sitemap()],
+  site: BASE_URL,
+  integrations: [
+    svelte(),
+    sitemap({
+      customPages: [BASE_URL + "/", BASE_URL + "/blog", ...getBlogs()],
+    }),
+  ],
+  markdown: {
+    shikiConfig: {
+      theme: "dark-plus",
+    },
+  },
+  output: "server",
+  adapter: netlify(),
 });
