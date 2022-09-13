a5c895d2e89e8e578686e24dcb40fe2467632b91
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-09-13 22:16:43 +0530

    patch: remove adapter netlify and use static adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import sitemap from "@astrojs/sitemap";
-import fs from "node:fs";
+/* import fs from "node:fs"; */
 
-import netlify from "@astrojs/netlify/functions";
+/* import netlify from "@astrojs/netlify/functions"; */
 const BASE_URL = "https://raqueebuddinaziz.com";
 
-function getBlogs() {
-  const files = fs.readdirSync("src/pages/blog");
-  const blogs = files.filter((file) => file.endsWith(".md"));
-  return blogs.map((blog) => BASE_URL + "/blog/" + blog.replace(".md", ""));
-}
+/* function getBlogs() { */
+/*   const files = fs.readdirSync("src/pages/blog"); */
+/*   const blogs = files.filter((file) => file.endsWith(".md")); */
+/*   return blogs.map((blog) => BASE_URL + "/blog/" + blog.replace(".md", "")); */
+/* } */
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,7 +19,7 @@ export default defineConfig({
   integrations: [
     svelte(),
     sitemap({
-      customPages: [BASE_URL + "/", BASE_URL + "/blog", ...getBlogs()],
+      /* customPages: [BASE_URL + "/", BASE_URL + "/blog", ...getBlogs()], */
     }),
   ],
   markdown: {
@@ -27,6 +27,6 @@ export default defineConfig({
       theme: "dark-plus",
     },
   },
-  output: "server",
-  adapter: netlify(),
+  /* output: "server", */
+  /* adapter: netlify(), */
 });
