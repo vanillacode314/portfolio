46edb055ab60dd8cbddcaefcba3e4aa5d3d1a4f4
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-09-14 15:03:11 +0530

    add astro plugins for performance+seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,27 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+import compress from "astro-compress";
+import critters from "astro-critters";
 /* import fs from "node:fs"; */
 
 /* import netlify from "@astrojs/netlify/functions"; */
-const BASE_URL = "https://raqueebuddinaziz.com";
 
+import compressor from "astro-compressor";
+import purgecss from "astro-purgecss";
+const BASE_URL = "https://raqueebuddinaziz.com";
 /* function getBlogs() { */
+
 /*   const files = fs.readdirSync("src/pages/blog"); */
+
 /*   const blogs = files.filter((file) => file.endsWith(".md")); */
+
 /*   return blogs.map((blog) => BASE_URL + "/blog/" + blog.replace(".md", "")); */
+
 /* } */
+// https://astro.build/config
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
@@ -21,6 +32,11 @@ export default defineConfig({
     sitemap({
       /* customPages: [BASE_URL + "/", BASE_URL + "/blog", ...getBlogs()], */
     }),
+    robotsTxt(),
+    purgecss(),
+    critters(),
+    compress(),
+    compressor(),
   ],
   markdown: {
     shikiConfig: {
@@ -28,5 +44,6 @@ export default defineConfig({
     },
   },
   /* output: "server", */
+
   /* adapter: netlify(), */
 });
