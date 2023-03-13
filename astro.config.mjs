ecae5048597a41fca163e5624b24e3ad75fd9070
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2023-03-13 12:11:10 +0530

    convert landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,34 @@
 import { defineConfig } from "astro/config";
 import Unocss from "unocss/astro";
-import { transformerDirectives, presetUno, presetWebFonts, presetIcons } from "unocss";
+import {
+  transformerDirectives,
+  presetUno,
+  presetWebFonts,
+  presetIcons,
+} from "unocss";
 
 // https://astro.build/config
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [Unocss({
-    transformers: [transformerDirectives()],
-    presets: [presetUno(), presetWebFonts({
-      fonts: {
-        sans: ["Inter:400,500,600,700,800,900"]
-      }
-    }), presetIcons()]
-  }), solidJs()]
+  integrations: [
+    image(),
+    Unocss({
+      transformers: [transformerDirectives()],
+      presets: [
+        presetUno(),
+        presetWebFonts({
+          fonts: {
+            sans: ["Inter:400,500,600,700,800,900"],
+          },
+        }),
+        presetIcons(),
+      ],
+    }),
+    solidJs(),
+  ],
 });
