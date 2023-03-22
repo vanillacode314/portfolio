c4cd8749583773a19ae37bc4d84ea5c9a44d4935
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2023-03-22 11:16:08 +0530

    fix form submission gtags

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,10 @@
 export const sleep = (duration: number) => new Promise((resolve) => setTimeout(resolve, duration))
+
+export function submitFormWithoutListener(
+	form: HTMLFormElement,
+	handler: (e: SubmitEvent) => void
+) {
+	form.removeEventListener('submit', handler)
+	form.submit()
+	form.addEventListener('submit', handler)
+}
