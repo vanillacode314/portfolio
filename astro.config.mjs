af67783b573d2e372815256d01c7d5d96fb856ad
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2023-07-08 15:15:49 +0530

    enable ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,73 +1,56 @@
-import image from '@astrojs/image'
-import { rehypeHeadingIds } from '@astrojs/markdown-remark'
-import partytown from '@astrojs/partytown'
-import sitemap from '@astrojs/sitemap'
-import solidJs from '@astrojs/solid-js'
-import compressor from 'astro-compressor'
-import robots from 'astro-robots-txt'
-import { defineConfig } from 'astro/config'
-import { h } from 'hastscript'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import rehypeToc from 'rehype-toc'
-import { presetIcons, presetUno, presetWebFonts, transformerDirectives } from 'unocss'
-import Unocss from 'unocss/astro'
+import image from '@astrojs/image';
+import { rehypeHeadingIds } from '@astrojs/markdown-remark';
+import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
+import solidJs from '@astrojs/solid-js';
+import compressor from 'astro-compressor';
+import robots from 'astro-robots-txt';
+import { defineConfig } from 'astro/config';
+import { h } from 'hastscript';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeToc from 'rehype-toc';
+import { presetIcons, presetUno, presetWebFonts, transformerDirectives } from 'unocss';
+import Unocss from 'unocss/astro';
+import mdx from '@astrojs/mdx';
 
-import mdx from '@astrojs/mdx'
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://raqueebuddinaziz.com/',
-	integrations: [
-		image(),
-		Unocss({
-			transformers: [transformerDirectives()],
-			presets: [
-				presetUno(),
-				presetWebFonts({
-					fonts: {
-						sans: ['Inter:400,500,600,700,800,900'],
-						mono: ['JetBrains Mono', 'monospace']
-					}
-				}),
-				presetIcons({
-					extraProperties: {
-						display: 'inline-block',
-						'vertical-align': 'middle'
-					}
-				})
-			],
-			safelist: ['i-mdi-link-variant']
-		}),
-		solidJs(),
-		sitemap(),
-		// partytown({
-		// 	config: {
-		// 		forward: ['dataLayer.push']
-		// 	}
-		// }),
-		robots(),
-		compressor(),
-		mdx()
-	],
-	markdown: {
-		shikiConfig: {
-			theme: 'dark-plus'
-		},
-		rehypePlugins: [
-			rehypeHeadingIds,
-			rehypeToc,
-			[
-				rehypeAutolinkHeadings,
-				{
-					content() {
-						return [
-							h('div.i-mdi-link-variant?bg.transition.hover:rotate-45.transform', {
-								ariaHidden: true
-							})
-						]
-					}
-				}
-			]
-		]
-	}
-})
+  site: 'https://raqueebuddinaziz.com/',
+  integrations: [image(), Unocss({
+    transformers: [transformerDirectives()],
+    presets: [presetUno(), presetWebFonts({
+      fonts: {
+        sans: ['Inter:400,500,600,700,800,900'],
+        mono: ['JetBrains Mono', 'monospace']
+      }
+    }), presetIcons({
+      extraProperties: {
+        display: 'inline-block',
+        'vertical-align': 'middle'
+      }
+    })],
+    safelist: ['i-mdi-link-variant']
+  }), solidJs(), sitemap(),
+  // partytown({
+  // 	config: {
+  // 		forward: ['dataLayer.push']
+  // 	}
+  // }),
+  robots(), compressor(), mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: 'dark-plus'
+    },
+    rehypePlugins: [rehypeHeadingIds, rehypeToc, [rehypeAutolinkHeadings, {
+      content() {
+        return [h('div.i-mdi-link-variant?bg.transition.hover:rotate-45.transform', {
+          ariaHidden: true
+        })];
+      }
+    }]]
+  },
+  output: "server",
+  adapter: netlify()
+});
