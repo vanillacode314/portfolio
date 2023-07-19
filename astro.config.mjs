3b09010d543b6c53e7fa811ad57c8475aecdc259
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2023-07-19 19:26:10 +0530

    fix sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,56 +1,89 @@
-import image from '@astrojs/image';
-import { rehypeHeadingIds } from '@astrojs/markdown-remark';
-import partytown from '@astrojs/partytown';
-import sitemap from '@astrojs/sitemap';
-import solidJs from '@astrojs/solid-js';
-import compressor from 'astro-compressor';
-import robots from 'astro-robots-txt';
-import { defineConfig } from 'astro/config';
-import { h } from 'hastscript';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypeToc from 'rehype-toc';
-import { presetIcons, presetUno, presetWebFonts, transformerDirectives } from 'unocss';
-import Unocss from 'unocss/astro';
-import mdx from '@astrojs/mdx';
+import image from '@astrojs/image'
+import { rehypeHeadingIds } from '@astrojs/markdown-remark'
+import mdx from '@astrojs/mdx'
+import partytown from '@astrojs/partytown'
+import sitemap from '@astrojs/sitemap'
+import solidJs from '@astrojs/solid-js'
+import compressor from 'astro-compressor'
+import robots from 'astro-robots-txt'
+import { defineConfig } from 'astro/config'
+import fs from 'fs'
+import { h } from 'hastscript'
+import path from 'path'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeToc from 'rehype-toc'
+import { presetIcons, presetUno, presetWebFonts, transformerDirectives } from 'unocss'
+import Unocss from 'unocss/astro'
 
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/functions'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const directoryPath = path.join(__dirname, 'src', 'content', 'blog')
+const files = fs.readdirSync(directoryPath)
+const siteUrl = 'https://raqueebuddinaziz.com'
+const blogUrls = files.map((file) => {
+	const fileName = file.split('.')[0]
+	return `${siteUrl}/blog/${fileName}/`
+})
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://raqueebuddinaziz.com/',
-  integrations: [image(), Unocss({
-    transformers: [transformerDirectives()],
-    presets: [presetUno(), presetWebFonts({
-      fonts: {
-        sans: ['Inter:400,500,600,700,800,900'],
-        mono: ['JetBrains Mono', 'monospace']
-      }
-    }), presetIcons({
-      extraProperties: {
-        display: 'inline-block',
-        'vertical-align': 'middle'
-      }
-    })],
-    safelist: ['i-mdi-link-variant']
-  }), solidJs(), sitemap(),
-  // partytown({
-  // 	config: {
-  // 		forward: ['dataLayer.push']
-  // 	}
-  // }),
-  robots(), compressor(), mdx()],
-  markdown: {
-    shikiConfig: {
-      theme: 'dark-plus'
-    },
-    rehypePlugins: [rehypeHeadingIds, rehypeToc, [rehypeAutolinkHeadings, {
-      content() {
-        return [h('div.i-mdi-link-variant?bg.transition.hover:rotate-45.transform', {
-          ariaHidden: true
-        })];
-      }
-    }]]
-  },
-  output: "server",
-  adapter: netlify()
-});
+	site: 'https://raqueebuddinaziz.com/',
+	integrations: [
+		image(),
+		Unocss({
+			transformers: [transformerDirectives()],
+			presets: [
+				presetUno(),
+				presetWebFonts({
+					fonts: {
+						sans: ['Inter:400,500,600,700,800,900'],
+						mono: ['JetBrains Mono', 'monospace']
+					}
+				}),
+				presetIcons({
+					extraProperties: {
+						display: 'inline-block',
+						'vertical-align': 'middle'
+					}
+				})
+			],
+			safelist: ['i-mdi-link-variant']
+		}),
+		solidJs(),
+		sitemap({ customPages: blogUrls }),
+		// partytown({
+		// 	config: {
+		// 		forward: ['dataLayer.push']
+		// 	}
+		// }),
+		robots(),
+		compressor(),
+		mdx()
+	],
+	markdown: {
+		shikiConfig: {
+			theme: 'dark-plus'
+		},
+		rehypePlugins: [
+			rehypeHeadingIds,
+			rehypeToc,
+			[
+				rehypeAutolinkHeadings,
+				{
+					content() {
+						return [
+							h('div.i-mdi-link-variant?bg.transition.hover:rotate-45.transform', {
+								ariaHidden: true
+							})
+						]
+					}
+				}
+			]
+		]
+	},
+	output: 'server',
+	adapter: netlify()
+})
