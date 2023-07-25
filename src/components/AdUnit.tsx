6b8a2c5c703a678ff0c81eca20a0675dd86c24fd
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2023-07-25 06:52:42 +0530

    fix ads

--- src/components/AdUnit.tsx
+++ src/components/AdUnit.tsx
@@ -1,16 +1,36 @@
 import { createMediaQuery } from '@solid-primitives/media'
-import { Show, type VoidComponent } from 'solid-js'
+import { Show, createEffect, onMount, type VoidComponent } from 'solid-js'
 
 export const AdUnit: VoidComponent = () => {
 	const isMobile = createMediaQuery('(max-width: 1024px)')
 
+	function initUnits() {
+		if (!isMobile()) return
+		const container = document.querySelector('.adsbygoogle')!
+		const w = document.write
+		document.write = function (content) {
+			container.innerHTML = content
+			document.write = w
+		}
+		const script = document.createElement('script')
+		script.async = true
+		script.crossOrigin = 'anonymous'
+		script.src =
+			'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3391473007789643'
+		document.body.appendChild(script)
+		;(window.adsbygoogle = window.adsbygoogle || []).push({})
+	}
+	createEffect(() => {
+		isMobile()
+		initUnits()
+	})
+
+	onMount(() => {
+		initUnits()
+	})
+
 	return (
-		<Show when={isMobile()}>
-			<script
-				async
-				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3391473007789643"
-				crossorigin="anonymous"
-			></script>
+		<Show when={!isMobile()}>
 			<ins
 				class="adsbygoogle"
 				style="display:block"
@@ -19,7 +39,6 @@ export const AdUnit: VoidComponent = () => {
 				data-ad-format="auto"
 				data-full-width-responsive="true"
 			></ins>
-			<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
 		</Show>
 	)
 }
