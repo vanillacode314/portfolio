6afb962b4a0364be5cd702ec587d80086c82d74a
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2023-07-31 16:50:14 +0530

    add meta pixels

--- src/components/Consent.tsx
+++ src/components/Consent.tsx
@@ -66,10 +66,32 @@ export const CookieConsent: VoidComponent<{ cookie: string }> = (props) => {
 			: 0
 	}
 
+	async function loadPixel() {
+		!(function (f, b, e, v, n, t, s) {
+			if (f.fbq) return
+			n = f.fbq = function () {
+				n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+			}
+			if (!f._fbq) f._fbq = n
+			n.push = n
+			n.loaded = !0
+			n.version = '2.0'
+			n.queue = []
+			t = b.createElement(e)
+			t.async = !0
+			t.src = v
+			s = b.getElementsByTagName(e)[0]
+			s.parentNode.insertBefore(t, s)
+		})(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js')
+		fbq('init', '988774449217223')
+		fbq('track', 'PageView')
+	}
+
 	onMount(() => {
 		loadAdsense()
 		if (!didConsent()) return
 		loadAnalytics()
+		loadPixel()
 	})
 
 	return (
