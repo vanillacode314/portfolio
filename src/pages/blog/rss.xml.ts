537d83927b4dbaf6561a78a320196168abefac3b
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-08-30 12:19:10 +0530

    update rss schema

--- src/pages/blog/rss.xml.ts
+++ src/pages/blog/rss.xml.ts
@@ -6,13 +6,23 @@ import sanitizeHtml from 'sanitize-html'
 const parser = new MarkdownIt()
 
 export const GET: APIRoute = async (context) => {
-	const posts = (await getCollection('blog')).sort((a, b) => {
-		const date1 = new Date(a.data.created)
-		const date2 = new Date(b.data.created)
-		if (date1 === date2) return 0
-		if (date1 >= date2) return -1
-		return 1
-	})
+	const posts = (await getCollection('blog'))
+		.sort((a, b) => {
+			const date1 = new Date(a.data.created)
+			const date2 = new Date(b.data.created)
+			if (date1 === date2) return 0
+			if (date1 >= date2) return -1
+			return 1
+		})
+		.map((post) => {
+			const updatedAt = new Date(post.data.updated || post.data.created)
+			return {
+				year: updatedAt.getFullYear().toString(),
+				month: (updatedAt.getMonth() + 1).toString().padStart(2, '0'),
+				day: updatedAt.getDate().toString().padStart(2, '0'),
+				...post
+			}
+		})
 
 	return rss({
 		stylesheet: '/assets/styles/rss.xsl',
@@ -24,7 +34,7 @@ export const GET: APIRoute = async (context) => {
 			pubDate: new Date(post.data.created),
 			description: post.data.description,
 			author: post.data.author,
-			link: `/blog/${post.slug}/`,
+			link: `/blog/${post.year}/${post.month}/${post.day}/${post.slug}/`,
 			content: sanitizeHtml(parser.render(post.body))
 		})),
 		customData: `<language>en-us</language>`
