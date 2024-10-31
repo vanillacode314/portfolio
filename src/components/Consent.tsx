d82007c3af4ec817d8e0dc41e2bd6026ae0ec218
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-10-31 23:03:08 +0530

    fix: gaScript

--- src/components/Consent.tsx
+++ src/components/Consent.tsx
@@ -56,19 +56,18 @@ export const CookieConsent: VoidComponent<{ cookie: string }> = (props) => {
       window.gtag = function gtag() {
         window.dataLayer.push(arguments)
       }
-      gtag('consent', 'default', {
-        ad_storage: 'denied',
-        ad_user_data: 'denied',
-        ad_personalization: 'denied',
-        analytics_storage: 'denied'
+      // gtag('consent', 'default', {
+      //   ad_storage: 'denied',
+      //   ad_user_data: 'denied',
+      //   ad_personalization: 'denied',
+      //   analytics_storage: 'denied'
+      // })
+      gtag('consent', 'update', {
+        ad_storage: 'granted',
+        ad_user_data: 'granted',
+        ad_personalization: 'granted',
+        analytics_storage: 'granted'
       })
-      if (didConsent())
-        gtag('consent', 'update', {
-          ad_storage: 'granted',
-          ad_user_data: 'granted',
-          ad_personalization: 'granted',
-          analytics_storage: 'granted'
-        })
 
       gtag('js', new Date())
       gtag('config', 'G-197W0VNG3Y', {
--- src/pages/meta-pixel-proxy/index.ts
+++ src/pages/meta-pixel-proxy/index.ts
@@ -1,5 +1,7 @@
 import type { APIRoute } from 'astro'
 
+export const prerender = false
+
 export const GET: APIRoute = async ({ url }) => {
 	const url2 = url.searchParams.get('url')
 	if (!url2) return new Response('url is required', { status: 400 })
