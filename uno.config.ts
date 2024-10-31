df84380cb881b44ff4eb54d02ecff6b2b4d96bad
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-10-31 21:10:36 +0530

    refactor: download webfonts locally

--- uno.config.ts
+++ uno.config.ts
@@ -1,4 +1,6 @@
-import { defineConfig, presetIcons, presetUno, presetWebFonts, transformerDirectives } from 'unocss'
+import presetWebFonts from '@unocss/preset-web-fonts'
+import { createLocalFontProcessor } from '@unocss/preset-web-fonts/local'
+import { defineConfig, presetIcons, presetUno, transformerDirectives } from 'unocss'
 
 export default defineConfig({
 	content: { filesystem: ['src/**/*.{astro,md,mdx,svelte,[tj]sx}'] },
@@ -8,9 +10,19 @@ export default defineConfig({
 		presetWebFonts({
 			provider: 'bunny',
 			fonts: {
-				sans: ['Ubuntu:400,500,600,700,800,900'],
+				sans: ['Ubuntu:400,500,600,700,800,900', 'sans-serif'],
 				mono: ['Ubuntu Mono', 'monospace']
-			}
+			},
+			processors: createLocalFontProcessor({
+				// Directory to cache the fonts
+				cacheDir: 'node_modules/.cache/unocss/fonts',
+
+				// Directory to save the fonts assets
+				fontAssetsDir: 'public/assets/fonts',
+
+				// Base URL to serve the fonts from the client
+				fontServeBaseUrl: '/assets/fonts'
+			})
 		}),
 		presetIcons({
 			extraProperties: {
