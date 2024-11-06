39a4d67f8dd0e86c4836c5d7fb49762d2247885d
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-11-06 19:15:08 +0530

    fix(umami): event name casing

--- src/sections/Comment.tsx
+++ src/sections/Comment.tsx
@@ -53,7 +53,7 @@ export default function Comments(props: { slug: string }) {
 
 	async function onSubmit(e: SubmitEvent) {
 		e.preventDefault()
-		window.umami.track('Comment', { slug: props.slug })
+		window.umami.track('comment', { slug: props.slug })
 
 		if (!validateComment()) {
 			alert('Your comment cannot contain html tags')
