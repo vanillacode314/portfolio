c3f9ebf215b9abffdc72232bbf07eb4d55742f5d
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-11-06 08:23:03 +0530

    feat(umami): track section scroll events

---
+++ src/utils/umami.ts
@@ -0,0 +1,24 @@
+function trackScroll(
+	id: string,
+	data: Record<string, string>,
+	{ thresholdPixels = 100 }: Partial<{ thresholdPixels: number }> = {}
+) {
+	const el = document.getElementById(id)
+	if (!el) {
+		if (import.meta.env.DEV) console.error('Element not found')
+		return
+	}
+	if (import.meta.env.DEV)
+		console.log('Scroll Tracking Activated', { id, el, data, thresholdPixels })
+	const onScroll = () => {
+		const scrollPos = window.scrollY - el.offsetTop
+		if (scrollPos > thresholdPixels) {
+			window.removeEventListener('scroll', onScroll)
+			umami.track('Scrolled Past By', { ...data, threshold: thresholdPixels })
+		}
+	}
+	window.addEventListener('scroll', onScroll)
+	return () => window.removeEventListener('scroll', onScroll)
+}
+
+export { trackScroll }
