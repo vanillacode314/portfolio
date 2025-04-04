75bf4cf4360f1dac41aa9c078111136146e1f5a1
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2025-04-04 19:35:46 +0530

    feat!: solar pivot

--- src/data/faqs.ts
+++ src/data/faqs.ts
@@ -6,23 +6,23 @@ export interface FAQ {
 export default [
 	{
 		summary: 'What guarantees do you provide?',
-		description:
-			'You get a 6-month bug fix guarantee. Any bugs within 6 months from when the contract was signed will be fixed free of charge.'
+		description: `<p>You get a <strong>6-month warranty for bug fixes</strong>. If any issues come up in the first 6 months after signing, I'll fix them for free.</p>
+<p>Also, I guarantee the automations will at least <strong>double your speed, saving you 50% or more of the time and costs it takes to do the work by hand</strong>.</p>`
 	},
-	{
-		summary: 'Are there any minimum budget requirements?',
-		description: 'Yes, your budget must be at least 500USD.'
-	},
-	{
-		summary: 'How does the process work?',
-		description: `<ol>
-                    <li>We discuss all the requirements of your project.</li>
-                    <li>Figure out the base features necessary for the MVP.</li>
-                    <li>We get back to you with a quote.</li>
-                    <li>Once a contract is in effect, we deliver the first version under 3days.</li>
-                    <li>If you want to continue building the app after the first version, we charge a weekly rate.</li>
-                  </ol>`
-	},
+	// {
+	// 	summary: 'Are there any minimum budget requirements?',
+	// 	description: 'Yes, your budget must be at least 500USD.'
+	// },
+	// {
+	// 	summary: 'How does the process work?',
+	// 	description: `<ol>
+	//                    <li>We discuss all the requirements of your project.</li>
+	//                    <li>Figure out the base features necessary for the MVP.</li>
+	//                    <li>We get back to you with a quote.</li>
+	//                    <li>Once a contract is in effect, we deliver the first version under 3days.</li>
+	//                    <li>If you want to continue building the app after the first version, we charge a weekly rate.</li>
+	//                  </ol>`
+	// },
 	{
 		summary: 'Do you provide ongoing support after the project is done?',
 		description: `<p>Yes, After the first version is built, you are free to choose either a maintenance only option or a full development option.</p> <p>The maintenance only option is at a rate of 100USD/month and the full development option is at a rate of 500USD, 1000USD or 1500USD per week depending on how much work you need done.</p>`
@@ -37,6 +37,6 @@ export default [
 	},
 	{
 		summary: 'Do you offer custom pricing?',
-		description: `Yes, if you think a custom rate would be more suitable for your project, we can work out a custom rate.`
+		description: `Yes, all quotes are given specifically for your business and solving your problem.`
 	}
 ] satisfies FAQ[]
