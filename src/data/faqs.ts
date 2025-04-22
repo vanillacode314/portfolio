716919349419454f537edff5e98dfdf03d60e101
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2025-04-23 00:49:57 +0530

    feat: update faq

--- src/data/faqs.ts
+++ src/data/faqs.ts
@@ -25,16 +25,17 @@ export default [
 	// },
 	{
 		summary: 'Do you provide ongoing support after the project is done?',
-		description: `<p>Yes, After the first version is built, you are free to choose either a maintenance only option or a full development option.</p> <p>The maintenance only option is at a rate of 100USD/month and the full development option is at a rate of 500USD, 1000USD or 1500USD per week depending on how much work you need done.</p>`
+		// description: `<p>Yes, After the first version is built, you are free to choose either a maintenance only option or a full development option.</p> <p>The maintenance only option is at a rate of 100USD/month and the full development option is at a rate of 500USD, 1000USD or 1500USD per week depending on how much work you need done.</p>`
+		description: `Yes`
 	},
-	{
-		summary: 'What is included in the maintenance only option?',
-		description: `The maintenance only option is limited to minor changes and bug fixes. Any major changes or additions will require a separate contract or the full development option. Maintenance fixes are capped at 10hrs/month.`
-	},
-	{
-		summary: 'What is included in the full development option?',
-		description: `The full development option works on a retainer basis. You get a max 10hrs/week of development time with the 500USD/week, a max of 20hrs/week with 1000USD/week, and a max of 30hrs/week with 1500USD/week.`
-	},
+	// {
+	// 	summary: 'What is included in the maintenance only option?',
+	// 	description: `The maintenance only option is limited to minor changes and bug fixes. Any major changes or additions will require a separate contract or the full development option. Maintenance fixes are capped at 10hrs/month.`
+	// },
+	// {
+	// 	summary: 'What is included in the full development option?',
+	// 	description: `The full development option works on a retainer basis. You get a max 10hrs/week of development time with the 500USD/week, a max of 20hrs/week with 1000USD/week, and a max of 30hrs/week with 1500USD/week.`
+	// },
 	{
 		summary: 'Do you offer custom pricing?',
 		description: `Yes, all quotes are given specifically for your business and solving your problem.`
